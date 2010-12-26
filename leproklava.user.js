853770692f5dff7c04d1472b89269270ba91b819
whitered <dmitry.zhelnin@gmail.com>
2010-12-26 20:37:32 +0300

    log container renamed to logDiv to work in google chrome

--- leproklava.user.js
+++ leproklava.user.js
@@ -923,30 +923,30 @@ function initNavigation()
 
 
 
-var log;
+var logDiv;
 function trace(msg)
 {
-  if(!log)
+  if(!logDiv)
   {
-    log = document.createElement("div");
-    log.style.position = "absolute";
-    log.style.width = "auto";
-    log.style.zIndex = "3";
-    log.style.right = "0em";
-    log.style.top = "0em";
-    log.style.backgroundColor = "#fcc";
-    log.style.fontSize = "12px";
-    log.style.padding = "1em";
+    logDiv = document.createElement("div");
+    logDiv.style.position = "absolute";
+    logDiv.style.width = "auto";
+    logDiv.style.zIndex = "3";
+    logDiv.style.right = "0em";
+    logDiv.style.top = "0em";
+    logDiv.style.backgroundColor = "#fcc";
+    logDiv.style.fontSize = "12px";
+    logDiv.style.padding = "1em";
     
     var body = document.getElementsByTagName("body")[0];
-    body.insertBefore(log, body.firstChild);
+    body.appendChild(logDiv);
   }
   
   var p = document.createElement("p");
   p.style.margin = "0em";
   
   p.appendChild(document.createTextNode(msg));
-  log.appendChild(p);
+  logDiv.appendChild(p);
 }
 
 
